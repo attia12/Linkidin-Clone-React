bc43e60b8deaa82269cfdbad7affb5ec96fd3caa
attia12 <attia1232020@gmail.com>
2024-09-20 14:08:35 +0200

    deplyement

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,27 @@ import notificationRoutes from "./routes/notification.route.js";
 import connectionRoutes from "./routes/connection.route.js";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 const app=express();
 dotenv.config();
 const PORT=process.env.PORT || 5000 ;
-app.use(cors({
-  origin:"http://localhost:5173", 
-  credentials:true,
-}))
+const __dirname = path.resolve();
+if(process.env.NODE_ENV !== "production")
+{
+  app.use(cors({
+    origin:"http://localhost:5173", 
+    credentials:true,
+  }))
+
+}
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
+
 app.use(express.json({limit:"5mb"})); // parse json request body
 app.use(cookieParser());
 app.use("/api/v1/auth",authRoutes);
